a019c416c14c0bb8d7cf3107a2177e87ba71af8f
EvelyneWauters <evelyne.wauters@gmail.com>
2015-05-26 11:07:31 +0200

    Initial settings setup

---
+++ src/main/java/be/vdab/blackbooks/Book.java
@@ -0,0 +1,61 @@
+package be.vdab.blackbooks;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Book implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String author;
+
+    private String isbn;
+
+    public Book() {
+    }
+
+    public Book(String author, String isbn, String title) {
+        this.author = author;
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
